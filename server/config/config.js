8525feda7b4ea672d6ab1a7693363fddc6940d06
bryan RR <bryan.rr@hotmail.com>
2018-10-29 20:15:46 -0500

    sss

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:NIRVANA33@ds245523.mlab.com:45523/cafe'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB;
